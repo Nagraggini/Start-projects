1b278e04accdc7412844305b7b955db359266a5f
Andrea Freud <62719085+Nagraggini@users.noreply.github.com>
2026-03-16 21:39:39 +0100

    Deleted old files.

--- java-console-intermediate/src/kosar2004Gyakorlas/GyakorlasSteamAPIKezdoSzint.java
+++ java-console-intermediate/src/kosar2004Gyakorlas/GyakorlasSteamAPIKezdoSzint.java
@@ -13,7 +13,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import static java.util.stream.Collectors.groupingBy;
 
-public class KosarligaGyakorlas {
+public class GyakorlasSteamAPIKezdoSzint {
 
     // https://infojegyzet.hu/vizsgafeladatok/okj-programozas/szoftverfejleszto-200526/
 
@@ -256,95 +256,5 @@ public class KosarligaGyakorlas {
         }
     }
 
-    /*
-     * TODO
-     * Könnyű → Közepes feladatok
-     * 7️⃣ Legnagyobb pontkülönbség
-     * 
-     * Találd meg azt a meccset ahol a legnagyobb pontkülönbség volt.
-     * 
-     * 8️⃣ Legkevesebb pont
-     * 
-     * Találd meg azt a meccset ahol a legkevesebb pont született összesen.
-     * 
-     * hazai_pont + idegen_pont
-     * 9️⃣ 2010 utáni meccsek
-     * 
-     * Írd ki az összes meccset ahol
-     * 
-     * idopont > 2010
-     * 🔟 200 pont feletti meccsek száma
-     * 
-     * Számold meg hány olyan meccs volt ahol
-     * 
-     * hazai_pont + idegen_pont > 200
-     * Kicsit nehezebb
-     * 11️⃣ Csapatok száma
-     * 
-     * Számold meg hány különböző csapat szerepel az adatbázisban
-     * (hazai + idegen együtt).
-     * 
-     * 12️⃣ Meccsek száma csapatonként
-     * 
-     * Készíts egy Map<String, Long> statisztikát ahol:
-     * 
-     * csapat -> hány meccset játszott
-     * 
-     * (Hazai + idegen együtt.)
-     * 
-     * 13️⃣ Legtöbb pontot szerző csapat hazai pályán
-     * 
-     * Találd meg azt a csapatot amelyik
-     * 
-     * hazai_pont összesen
-     * 
-     * alapján a legtöbb pontot dobta.
-     * 
-     * 14️⃣ Hány különböző helyszín van
-     * 
-     * Számold meg hány különböző:
-     * 
-     * helyszin
-     * 
-     * van az adatbázisban.
-     * 
-     * 15️⃣ Legtöbb meccset játszó csapat
-     * 
-     * Találd meg melyik csapat szerepel a legtöbb meccsen
-     * (hazai + idegen együtt).
-     */
-    /*
-     * 
-     * 
-     * Közepes feladatok
-     * 
-     * Átlagos hazai pont
-     * Számold ki az átlagos pontszámot, amit a hazai csapatok szereztek az összes
-     * meccsen.
-     * 
-     * Top 3 leggyőztes hazai csapat
-     * Találd meg a 3 legtöbbször nyerő hazai csapatot. (Hazai győzelem = hazai_pont
-     * > idegen_pont)
-     * 
-     * Pontkülönbség nagyobb mint 10
-     * Listázd ki azokat a meccseket, ahol a győztes legalább 10 ponttal nyert.
-     * 
-     * Összes pont csapatokra bontva
-     * Készíts egy Map<String, Integer>-t, ahol minden csapat nevehez az összes
-     * szerzett pont van hozzáadva (hazai és idegen meccsek együtt).
-     * 
-     * Nehéz feladatok
-     * 
-     * Legjobb helyszín
-     * Találd meg azt a helyszínt, ahol a legtöbb meccset játszották.
-     * 
-     * Legtöbb győzelmet szerző csapat
-     * Számold meg melyik csapat nyerte a legtöbb meccset (hazai + idegen győzelmek
-     * együtt).
-     * 
-     * Top 3 legmagasabb pontszámú meccs
-     * Találd meg a 3 meccset a legmagasabb összpontszámmal (hazai_pont +
-     * idegen_pont), és rendezd csökkenő sorrendbe.
-     */
-
+    
 }
---
+++ java-console-intermediate/src/kosar2004Gyakorlas/GyakorlasSteamAPIKozepesSzint.java
@@ -0,0 +1,106 @@
+package kosar2004Gyakorlas;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class GyakorlasSteamAPIKozepesSzint {
+
+    // https://infojegyzet.hu/vizsgafeladatok/okj-programozas/szoftverfejleszto-200526/
+
+    // Puska: https://github.com/Nagraggini/blog/blob/main/Java_basic_knowledge.md
+
+    public static ArrayList<AbcKosarlabdaLiga> lista = new ArrayList<>();
+    public static HashMap<Integer, AbcKosarlabdaLiga> hmap = new HashMap<>();
+
+    /*
+     * // TODO
+     * Könnyű → Közepes feladatok
+     * 7️⃣ Legnagyobb pontkülönbség
+     * 
+     * Találd meg azt a meccset ahol a legnagyobb pontkülönbség volt.
+     * 
+     * 8️⃣ Legkevesebb pont
+     * 
+     * Találd meg azt a meccset ahol a legkevesebb pont született összesen.
+     * 
+     * hazai_pont + idegen_pont
+     * 9️⃣ 2010 utáni meccsek
+     * 
+     * Írd ki az összes meccset ahol
+     * 
+     * idopont > 2010
+     * 🔟 200 pont feletti meccsek száma
+     * 
+     * Számold meg hány olyan meccs volt ahol
+     * 
+     * hazai_pont + idegen_pont > 200
+     * Kicsit nehezebb
+     * 11️⃣ Csapatok száma
+     * 
+     * Számold meg hány különböző csapat szerepel az adatbázisban
+     * (hazai + idegen együtt).
+     * 
+     * 12️⃣ Meccsek száma csapatonként
+     * 
+     * Készíts egy Map<String, Long> statisztikát ahol:
+     * 
+     * csapat -> hány meccset játszott
+     * 
+     * (Hazai + idegen együtt.)
+     * 
+     * 13️⃣ Legtöbb pontot szerző csapat hazai pályán
+     * 
+     * Találd meg azt a csapatot amelyik
+     * 
+     * hazai_pont összesen
+     * 
+     * alapján a legtöbb pontot dobta.
+     * 
+     * 14️⃣ Hány különböző helyszín van
+     * 
+     * Számold meg hány különböző:
+     * 
+     * helyszin
+     * 
+     * van az adatbázisban.
+     * 
+     * 15️⃣ Legtöbb meccset játszó csapat
+     * 
+     * Találd meg melyik csapat szerepel a legtöbb meccsen
+     * (hazai + idegen együtt).
+     */
+    /*
+     * 
+     * 
+     * Közepes feladatok
+     * 
+     * Átlagos hazai pont
+     * Számold ki az átlagos pontszámot, amit a hazai csapatok szereztek az összes
+     * meccsen.
+     * 
+     * Top 3 leggyőztes hazai csapat
+     * Találd meg a 3 legtöbbször nyerő hazai csapatot. (Hazai győzelem = hazai_pont
+     * > idegen_pont)
+     * 
+     * Pontkülönbség nagyobb mint 10
+     * Listázd ki azokat a meccseket, ahol a győztes legalább 10 ponttal nyert.
+     * 
+     * Összes pont csapatokra bontva
+     * Készíts egy Map<String, Integer>-t, ahol minden csapat nevehez az összes
+     * szerzett pont van hozzáadva (hazai és idegen meccsek együtt).
+     * 
+     * Nehéz feladatok
+     * 
+     * Legjobb helyszín
+     * Találd meg azt a helyszínt, ahol a legtöbb meccset játszották.
+     * 
+     * Legtöbb győzelmet szerző csapat
+     * Számold meg melyik csapat nyerte a legtöbb meccset (hazai + idegen győzelmek
+     * együtt).
+     * 
+     * Top 3 legmagasabb pontszámú meccs
+     * Találd meg a 3 meccset a legmagasabb összpontszámmal (hazai_pont +
+     * idegen_pont), és rendezd csökkenő sorrendbe.
+     */
+
+}
